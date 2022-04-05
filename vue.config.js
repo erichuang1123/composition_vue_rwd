06d4fc9dd6d2358129494ae8d13d858faa95447a
zhihao <andyhuang5003@gmail.com>
2022-04-05 19:27:55 +0800

    vue.config.js 修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath:'./',
+        configureWebpack:{
+          resolve: {
+              alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views',
+                'plugins': '@/plugins',
+              }
+        }   
+  }
 })
