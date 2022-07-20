aeb4897e57750627bcc9f5deca5bdff4bdeeec3b
zhihao <andyhuang5003@gmail.com>
2022-07-20 15:42:52 +0800

    deploy

---
+++ js/about.09e5f5d4.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_rwd"]=self["webpackChunkvue_rwd"]||[]).push([[443],{7381:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var r=e(3396);const t={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),s=[a];function c(n,u){return(0,r.wg)(),(0,r.iD)("div",t,s)}var i=e(89);const o={},d=(0,i.Z)(o,[["render",c]]);var f=d}}]);
+//# sourceMappingURL=about.09e5f5d4.js.map
